fa19a233998bc81f1b014655282780e366c151a8
Matija Milosevic <matijamilosevic1988@gmail.com>
2020-12-30 21:39:22 +0100

    Added React Router and general sidebar styles.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import Header from "./components/header/Header";
+import Sidebar from "./components/sidebar/Sidebar";
+
+import Home from "./components/home/Home";
+import Trending from "./components/trending/Trending";
+import Subscriptions from "./components/subscriptions/Subscriptions";
 
 function App() {
   return (
     <div className='app'>
       <Header />
+      <div className='app__body'>
+        <Router>
+          <Sidebar />
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/trending' component={Trending} />
+            <Route path='/subscriptions' component={Subscriptions} />
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
