32bbc0becd038585f09f7349cdcbcd295883bac8
Matija Milosevic <matijamilosevic1988@gmail.com>
2021-01-07 12:34:47 +0100

    Added Chips Bar and Slider carousel.

--- src/components/chipsbar/Chips.js
+++ src/components/chipsbar/Chips.js
@@ -1,10 +1,27 @@
 import React from "react";
 import "./Chips.css";
 
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 export default function Chips({ title, activeChip }) {
+  function handleClick(e) {
+    let chip = e.target;
+    let chips = document.querySelectorAll(".chips");
+
+    chips.forEach((chip) => {
+      chip.classList.remove("chips-active");
+    });
+
+    chip.classList.add("chips-active");
+  }
+
   return (
     <div>
-      <button className={`chips ${activeChip && "chips-active"}`}>{title}</button>
+      <button onClick={handleClick} className={`chips ${activeChip && "chips-active"}`}>
+        {title}
+      </button>
     </div>
   );
 }
--- src/components/chipsbar/Chipsbar.js
+++ src/components/chipsbar/Chipsbar.js
@@ -3,23 +3,42 @@ import "./Chipsbar.css";
 
 import Chips from "./Chips";
 
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 function Chipsbar() {
+  var settings = {
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    outerEdgeLimit: true,
+    arrows: false,
+  };
   return (
     <div className='chipsbar'>
-      <Chips title='All' activeChip={true} />
-      <Chips title='Chill-out-music' />
-      <Chips title='Jeremy Soule' />
-      <Chips title='NBA' />
-      <Chips title='Ambient Music' />
-      <Chips title='CSS' />
-      <Chips title='JavaScript' />
-      <Chips title='Trailers' />
-      <Chips title='Pop Music' />
-      <Chips title='Recently Uploaded' />
-      <Chips title='Live' />
-      <Chips title='Computer Science' />
-      <Chips title='First Take' />
-      <Chips title='HTML' />
+      <Slider className='slide' {...settings}>
+        <Chips title='All' activeChip={true} />
+        <Chips title='Chill-out-music' />
+        <Chips title='Jeremy Soule' />
+        <Chips title='NBA' />
+        <Chips title='Ambient Music' />
+        <Chips title='CSS' />
+        <Chips title='JavaScript' />
+        <Chips title='Trailers' />
+        <Chips title='Pop Music' />
+        <Chips title='Recently Uploaded' />
+        <Chips title='Live' />
+        <Chips title='Computer Science' />
+        <Chips title='First Take' />
+        <Chips title='HTML' />
+        <Chips title='Instrumentals' />
+        <Chips title='Website' />
+        <Chips title='Nature' />
+        <Chips title='Cars' />
+      </Slider>
     </div>
   );
 }
